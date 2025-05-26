c3289063d96f19ec3a524d53c1820ff652b6f5b6
Pavel Safonov <p.n.safonov@gmail.com>
2025-05-26 12:45:31 +0300

    refactoring

--- core/libc/libc.go
+++ core/libc/libc.go
@@ -7,30 +7,6 @@ import (
 	"unsafe"
 )
 
-//type ExitStatus struct {
-//	ETermination int32
-//	EExit        int32
-//}
-//
-//type UTTVType struct {
-//	TVSec  int32
-//	TVUsec int32
-//}
-//
-//type UTMP struct {
-//	UTType    uint16
-//	UTPid     int32
-//	UTLine    [32]byte
-//	UTId      [4]byte
-//	UTUser    [32]byte
-//	UTHost    [256]byte
-//	UTExit    ExitStatus
-//	UTSession int32
-//	UTTV      UTTVType
-//	UTAddrV6  [4]int32
-//	Reserved  [20]byte
-//}
-
 type ExitStatus struct {
 	Termination int16
 	Exit        int16
@@ -54,31 +30,14 @@ type UTMP struct {
 
 // Getutent - extern struct utmp *getutent (void) __THROW;
 func Getutent0() []*UTMP {
-	//var (
-	//	result0 uintptr
-	//)
-	//result0 = C.getutent()
-	//if result0 == 0 {
-	//	return
-	//}
-
 	result := make([]*UTMP, 0)
 	for {
-		//utmp0 := uintptr(0)
-		// _Cfunc_getutent)() (value of type *_Ctype_struct_utmp)
 		utmp0 := C.getutent()
 		utmp1 := uintptr(unsafe.Pointer(utmp0))
 		if utmp1 == 0 {
 			break
 		}
 
-		//utmp1 := (*UTMP)(unsafe.Pointer(utmp0))
-		//result = append(result, utmp1)
-		//
-		//i0 := int(utmp0.ut_type)
-		//fmt.Printf("i0 = %d\n", i0)
-
-		//line := C.GoStringN(utmp0.ut_line, C.UT_LINESIZE)
 		line := C.GoStringN(&utmp0.ut_line[0], C.UT_LINESIZE)
 		id := C.GoStringN(&utmp0.ut_id[0], 4)
 		user := C.GoStringN(&utmp0.ut_user[0], C.UT_NAMESIZE)
