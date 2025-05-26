8c92139b1942d03fc2736edb0ab543432aa8df35
Pavel Safonov <p.n.safonov@gmail.com>
2025-05-26 11:33:55 +0300

    try golang libc lib

--- logic/logic.go
+++ logic/logic.go
@@ -1,5 +1,11 @@
 package logic
 
+import (
+	"fmt"
+	libc0 "modernc.org/libc"
+	"unsafe"
+)
+
 const (
 	/* DefaultLogFile = "/var/log/inact/inact.log" */
 
@@ -13,5 +19,30 @@ type Context struct {
 }
 
 func DoRun(ctx *Context) error {
+
+	tls := libc0.NewTLS()
+	defer tls.Close()
+
+	for i := 0; true; i++ {
+		utp0 := libc0.Xgetutent(tls)
+		if utp0 == 0 {
+			break
+		}
+		utmp1 := unsafe.Pointer(utp0)
+		utmp2 := (*libc0.Tutmpx)(utmp1)
+
+		fmt.Printf("i = %d, ut_type = %d, tv_sec = %d, ut_id = %s, ut_pid = %d, ut_user = %s, ut_line = %s\n",
+			i,
+			utmp2.Fut_type,
+			utmp2.Fut_tv.Ftv_sec,
+			utmp2.Fut_id,
+			utmp2.Fut_pid,
+			utmp2.Fut_user,
+			utmp2.Fut_line,
+		)
+
+		//utmp2.
+	}
+
 	return nil
 }
